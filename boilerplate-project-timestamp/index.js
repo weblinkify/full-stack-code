3bc8293243c9ceb756918bc99ccd4d2ec7c2bbb4
weblinkify <daniyaltariq09@gmail.com>
2026-08-20 12:03:57 +0300

    Complete Timestamp Microservice

--- boilerplate-project-timestamp/index.js
+++ boilerplate-project-timestamp/index.js
@@ -8,24 +8,24 @@ app.use(cors({ optionsSuccessStatus: 200 }));
 app.use(express.static('public'));
 
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-// Timestamp Microservice
-app.get("/api/timestamp/:date?", function (req, res) {
-  var dateInput = req.params.date;
-
+// Timestamp API
+app.get("/api/:date?", function (req, res) {
   var date;
 
-  // No date supplied
-  if (!dateInput) {
+  // No date provided
+  if (!req.params.date) {
     date = new Date();
-  } else if (/^\d+$/.test(dateInput)) {
-    // Unix timestamp in milliseconds
-    date = new Date(Number(dateInput));
-  } else {
-    // Date string
-    date = new Date(dateInput);
+  } 
+  // Unix timestamp
+  else if (/^\d+$/.test(req.params.date)) {
+    date = new Date(parseInt(req.params.date));
+  } 
+  // Date string
+  else {
+    date = new Date(req.params.date);
   }
 
   // Invalid date
@@ -42,9 +42,8 @@ app.get("/api/timestamp/:date?", function (req, res) {
   });
 });
 
-// Listen on port
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log(
-    'Your app is listening on port ' + listener.address().port
+    "Your app is listening on port " + listener.address().port
   );
 });
