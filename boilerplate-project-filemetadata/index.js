3578a4f3ceabb78e8694c85de646c13d0de55a24
weblinkify <daniyaltariq09@gmail.com>
2026-08-20 12:23:56 +0300

    File Metadata Microservice v2

--- boilerplate-project-filemetadata/index.js
+++ boilerplate-project-filemetadata/index.js
@@ -14,10 +14,8 @@ app.get('/', (req, res) => {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Multer
 const upload = multer({ dest: 'uploads/' });
 
-// File upload endpoint
 app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
   res.json({
     name: req.file.originalname,
